7612c96308446194f7d5533d8ea88a4835471fa9
Passawat Noraman <hi@notrealpaz.me>
2025-03-11 00:05:08 +0700

    Simple Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+// server.ts
+import { Elysia, t } from "elysia";
+
+const app = new Elysia()
+	.get("/", () => "Hi Elysia")
+	.get("/health", () => "alive")
+	.get("/id/:id", ({ params: { id } }) => id)
+	.post("/mirror", ({ body }) => body, {
+		body: t.Object({
+			id: t.Number(),
+			name: t.String(),
+		}),
+	})
+	.listen(3000);
+
+export type App = typeof app;
